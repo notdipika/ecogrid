6a8366ec9de687f3e0b4a53de5a0fdbf324a0230
riyooo13 <chaudhary.dipika17@gmail.com>
2025-01-26 05:14:14 +0545

    backend update

--- EcoGrid/EcoGrid.ino
+++ EcoGrid/EcoGrid.ino
@@ -2,44 +2,207 @@
 #include <DHT.h>
 #include <Wire.h>
 #include <HTTPClient.h>
-#include <LiquidCrystal_I2C.h>  
+#include <LiquidCrystal_I2C.h>
 
-const char* ssid = "your_wifi_ssid";
-const char* password = "your_wifi_password";
-const char* serverName =  "http://127.0.0.1/sensor_data";
+// WiFi credentials
+const char* ssid = "titan";
+const char* password = "teamtitan";
 
-DHT dht(4, DHT11);    
+// Server URLs
+const char* commandServer = "http://192.168.1.100:5000/get_command";
+const char* sensorDataServer = "http://192.168.1.100:5000/sensor_data";
+const char* alertServer = "http://192.168.1.100:5000/alert";
 
-#define MQ2_SENSOR_PIN 34  
+// Pins
+#define ULTRASONIC_TRIG_PIN 19
+#define ULTRASONIC_ECHO_PIN 21
+#define MQ2_SENSOR_PIN 34
+#define PIR_SENSOR_PIN 2
+#define LED_PIN 32
+#define DHT_PIN 4
+#define LIGHT_PIN 23
 
-int gasThreshold = 35;  
+// Thresholds
+int gasThreshold = 35;
+int waterLevelThreshold = 10; // Water level threshold (in cm)
 
+// DHT sensor setup
+DHT dht(DHT_PIN, DHT11);
+
+// LCD setup
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  pinMode(LIGHT_PIN, OUTPUT);
+  pinMode(PIR_SENSOR_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(ULTRASONIC_TRIG_PIN, OUTPUT);
+  pinMode(ULTRASONIC_ECHO_PIN, INPUT);
+
+  digitalWrite(LIGHT_PIN, LOW); 
+  digitalWrite(LED_PIN, LOW);
+
   dht.begin();
   lcd.begin(16, 2);
-  lcd.backlight();  
+  lcd.backlight();
 
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-
   Serial.println("Connected to WiFi");
 }
 
 void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    fetchCommand();
+    sendSensorData();
+    checkMotion();
+    checkWaterLevel();
+    fetchCommand();
+  } else {
+    Serial.println("WiFi disconnected. Reconnecting...");
+    WiFi.reconnect();
+  }
+
+  displaySensorData();
+  delay(10000); // Main loop delay
+}
+
+void fetchCommand() {
+  HTTPClient http;
+  http.begin(commandServer);
+  int httpResponseCode = http.GET();
+
+  if (httpResponseCode > 0) {
+    String command = http.getString();
+    command.trim();
+    Serial.println("Received command: " + command);
+    handleCommand(command);
+  } else {
+    Serial.println("Error fetching command");
+  }
+
+  http.end();
+}
+
+void handleCommand(String command) {
+  command.toLowerCase();
+  if (command == "turn on the light") {
+    digitalWrite(LIGHT_PIN, HIGH);
+    Serial.println("Light turned ON");
+  } else if (command == "turn off the light") {
+    digitalWrite(LIGHT_PIN, LOW);
+    Serial.println("Light turned OFF");
+  } else {
+    Serial.println("Unknown command");
+  }
+}
+
+void sendSensorData() {
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
+  int gasValue = analogRead(MQ2_SENSOR_PIN);
 
-  int gasValue = analogRead(MQ2_SENSOR_PIN);  
-
-  if (gasValue > gasThreshold || temperature > 30) {  
+  if (gasValue > gasThreshold || temperature > 30) {
     sendAlert(gasValue, temperature);
   }
+
+  HTTPClient http;
+  http.begin(sensorDataServer);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  String httpRequestData = "temperature=" + String(temperature) + "&humidity=" + String(humidity);
+  int httpResponseCode = http.POST(httpRequestData);
+
+  if (httpResponseCode > 0) {
+    Serial.println("Sensor data sent successfully");
+  } else {
+    Serial.println("Error sending sensor data");
+  }
+
+  http.end();
+}
+
+void sendAlert(int gasLevel, float temp) {
+  HTTPClient http;
+  String alertMessage = "Alert! ";
+
+  if (gasLevel > gasThreshold) {
+    alertMessage += "Gas leak detected. ";
+  }
+  if (temp > 30) {
+    alertMessage += "High temperature detected. ";
+  }
+
+  http.begin(alertServer);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  String httpRequestData = "alert=" + alertMessage;
+  int httpResponseCode = http.POST(httpRequestData);
+
+  if (httpResponseCode > 0) {
+    Serial.println("Alert sent successfully");
+  } else {
+    Serial.println("Error sending alert");
+  }
+
+  http.end();
+}
+
+void checkMotion() {
+  int motionDetected = digitalRead(PIR_SENSOR_PIN);
+  if (motionDetected == HIGH) {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("Motion detected: LED ON");
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("No motion: LED OFF");
+  }
+}
+
+void checkWaterLevel() {
+  long duration, distance;
+
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC_TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC_TRIG_PIN, LOW);
+
+  duration = pulseIn(ULTRASONIC_ECHO_PIN, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  if (distance <= waterLevelThreshold) {
+    sendWaterAlert();
+  }
+}
+
+void sendWaterAlert() {
+  HTTPClient http;
+  String alertMessage = "Alert! Water level detected in the tank.";
+
+  http.begin(alertServer);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  String httpRequestData = "alert=" + alertMessage;
+  int httpResponseCode = http.POST(httpRequestData);
+
+  if (httpResponseCode > 0) {
+    Serial.println("Water alert sent successfully");
+  } else {
+    Serial.println("Error sending water alert");
+  }
+
+  http.end();
+}
+
+void displaySensorData() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -51,55 +214,4 @@ void loop() {
   lcd.print("Humidity: ");
   lcd.print(humidity);
   lcd.print(" %");
-
-  if(WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(serverName);
-
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    String httpRequestData = "ldr=" + String(ldrValue) + "&temperature=" + String(temperature) + "&humidity=" + String(humidity);
-    int httpResponseCode = http.POST(httpRequestData);
-
-    if(httpResponseCode > 0) {
-      Serial.println("Data Sent Successfully");
-    } else {
-      Serial.println("Error sending data");
-    }
-
-    http.end();
-  }
-
-  delay(10000); 
-}
-
-void sendAlert(int gasLevel, float temp) {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    String alertMessage = "Alert! ";
-
-    if (gasLevel > gasThreshold) {
-      alertMessage += "Gas leak detected. ";
-    }
-
-    if (temp > 30) {
-      alertMessage += "High temperature detected. ";
-    }
-
-    String serverAlertURL = "http://127.0.0.1/sensor_data/alert";  
-    http.begin(serverAlertURL);
-
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    String httpRequestData = "alert=" + alertMessage;
-    int httpResponseCode = http.POST(httpRequestData);
-
-    if (httpResponseCode > 0) {
-      Serial.println("Alert sent successfully");
-    } else {
-      Serial.println("Error sending alert");
-    }
-
-    http.end();
-  }
 }
--- app.py
+++ app.py
@@ -29,6 +29,55 @@ def init_db():
     conn.commit()
     conn.close()
 
+# Variable to store the latest command
+latest_command = ""
+
+@app.route("/send_command", methods=["POST"])
+def send_command():
+    global latest_command
+    command = request.form.get("command")
+    if command:
+        latest_command = command
+        return "Command received", 200
+    return "No command provided", 400
+
+@app.route("/get_command", methods=["GET"])
+def get_command():
+    global latest_command
+    return latest_command, 200
+
+@app.route("/sensor_data", methods=["POST"])
+def sensor_data():
+    ldr = request.form.get("ldr")
+    temperature = request.form.get("temperature")
+    humidity = request.form.get("humidity")
+
+    if not all([ldr, temperature, humidity]):
+        return "Missing sensor data", 400
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO sensor_data (ldr, temperature, humidity) VALUES (?, ?, ?)", (ldr, temperature, humidity))
+    conn.commit()
+    conn.close()
+
+    return "Sensor data received", 200
+
+@app.route("/alert", methods=["POST"])
+def alert():
+    alert_message = request.form.get("alert")
+
+    if not alert_message:
+        return "No alert message provided", 400
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO alerts (message) VALUES (?)", (alert_message,))
+    conn.commit()
+    conn.close()
+
+    return "Alert received", 200
+
 @app.route("/")
 def index():
     # Fetch latest sensor data
@@ -43,32 +92,6 @@ def index():
     conn.close()
 
     return render_template("index.html", data=latest_data, alerts=latest_alerts)
-
-@app.route("/sensor_data", methods=["POST"])
-def sensor_data():
-    ldr = request.form.get("ldr")
-    temperature = request.form.get("temperature")
-    humidity = request.form.get("humidity")
-
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO sensor_data (ldr, temperature, humidity) VALUES (?, ?, ?)", (ldr, temperature, humidity))
-    conn.commit()
-    conn.close()
-
-    return "Sensor data received", 200
-
-@app.route("/alert", methods=["POST"])
-def alert():
-    alert_message = request.form.get("alert")
-
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO alerts (message) VALUES (?)", (alert_message,))
-    conn.commit()
-    conn.close()
-
-    return "Alert received", 200
 
 if __name__ == "__main__":
     init_db()
